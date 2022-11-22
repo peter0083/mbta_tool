21ccb66f92dbe5fe8513096720d151768892a9c4
peter0083 <peter.tingyao@gmail.com>
2022-11-22 16:35:43 +0800

    bug: fix mbta_cli logics

--- mbta_cli.py
+++ mbta_cli.py
@@ -2,25 +2,31 @@ import argparse
 import get_route
 from pprint import pprint
 import logging
+import sys
 
 
-def main() -> int:
+def main(args=sys.argv[1:]) -> int:
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser(prog="mbta_tool", description="simple cli tool to call mbta api")
-    parser.add_argument("-ls_routes", "--list_subway_routes", action="store_true",
-                        help="List all subway routes.")
-    parser.add_argument("-ls_route_ids", "--list_subway_route_ids", action="store_true",
-                        help="List all subway routes and their IDs.")
-    parser.add_argument("from_station", type=str, help="Show route from station A to station B.")
-    parser.add_argument("to_station", type=str, help="Show route from station A to station B.")
-    args = parser.parse_args()
+    if args and (args[0].startswith("--") or args[0].startswith("-")):
+        parser.add_argument("-ls_routes", "--list_subway_routes", action="store_true",
+                            help="List all subway routes.")
+        parser.add_argument("-ls_route_ids", "--list_subway_route_ids", action="store_true",
+                            help="List all subway routes and their IDs.")
+        args1 = parser.parse_args()
 
-    if args.list_subway_routes:
-        pprint(get_route.get_subway_route_long_name())
-    if args.list_subway_route_ids:
-        pprint(get_route.get_subway_route_id())
-    if args.from_station and args.to_station:
-        pprint(get_route.show_subway_route(args.from_station, args.to_station))
+        if args1.list_subway_route_ids:
+            pprint(get_route.get_subway_route_id())
+        if args1.list_subway_routes:
+            pprint(get_route.get_subway_route_long_name())
+
+    else:
+        parser.add_argument("from_station", type=str, help="Show route from station A to station B.")
+        parser.add_argument("to_station", type=str, help="Show route from station A to station B.")
+        args2 = parser.parse_args()
+
+        if args2.from_station and args2.to_station:
+            pprint(get_route.show_subway_route(args2.from_station, args2.to_station))
 
     return 0
 
